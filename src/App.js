2b4ab0d5cb9b002fca0c9f75eb41ee6a3e7536e9
Eduardogrq <eduardo.grquinonez@gmail.com>
2022-08-16 22:24:57 -0500

    Creating react-redux states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export const reducer = (state = 0, action) => {
+  console.log({state, action});
+  switch (action.type) {
+    case 'incrementar':
+      return state + 1
+    case 'decrementar':
+      return state - 1
+    case 'set':
+      return action.payload
+  }
+}
+
+const App = () => {
+
+  const [valor, setValor] = useState('')
+  const state = useSelector(state => state)
+  const dispatch = useDispatch();
+  const set = () => {
+    dispatch({type: 'set', payload: valor})
+    setValor('')
+  }
+
+  return(
+    <div>
+      <p>contador: {state ? state : 0}</p>
+      <button onClick={() => dispatch({type: 'incrementar'})}>Incrementar</button>
+      <button onClick={() => dispatch({type: 'decrementar'})}>Decrementar</button>
+      <button onClick={set}>Set</button>
+      <input type="number" value={valor} onChange={ e => setValor(Number(e.target.value)) }></input>
     </div>
-  );
+  )
 }
 
 export default App;
